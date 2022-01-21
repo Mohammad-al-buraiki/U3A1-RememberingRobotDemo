c25830aa2495ac89085ef48f4a2100b67bfb9cbc
Mohammad Al-Buraiki <74559902+Mohammad-al-buraiki@users.noreply.github.com>
2022-01-21 11:47:40 -0500

    Create RobotMain.java

---
+++ RobotMain.java
@@ -0,0 +1,18 @@
+/**
+ * Main class
+ * @author Mohammad Al-Buraiki
+ * @version December 2021
+ */
+public class RobotMain
+{
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		RobotTask task = new RobotTask();
+		task.run();
+	}
+}
